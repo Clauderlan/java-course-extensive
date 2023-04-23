aa204c244fa13dbcd212f3eb501828c3316b263e
c7 <89857887+Clauderlan@users.noreply.github.com>
2023-04-23 16:57:08 -0300

    Section 21 - JDBC - update

---
+++ JDBC/update/src/application/Program.java
@@ -0,0 +1,35 @@
+package JDBC.update.src.application;
+
+import JDBC.insert.src.db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try{
+            connection = DB.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE (DepartmentId = ?)"
+            );
+            preparedStatement.setDouble(1, 200);
+            preparedStatement.setInt(2, 2);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            System.out.println("rows affected : " + rowsAffected);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
+
+    }
+}
